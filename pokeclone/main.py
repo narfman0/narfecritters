64ba72a7103bb3538a053b238db2cc6eff17854e
Jon Robison <narfman0@gmail.com>
2023-04-20 15:12:12 -0400

    initial commit

---
+++ pokeclone/main.py
@@ -0,0 +1,19 @@
+"""
+video based autosplitter for smb3
+"""
+import logging
+
+from pokeclone import settings
+
+
+from pokeclone.logging import initialize_logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+def main():
+    initialize_logging()
+
+
+if __name__ == "__main__":
+    main()
