0a2116b480f0624478f9852ec90adf74bcc070a8
Jon Robison <narfman0@gmail.com>
2023-05-07 23:31:59 -0400

    fix buy bug, show items in buy screen

--- narfecritters/ui/merchant/buy_screen.py
+++ narfecritters/ui/merchant/buy_screen.py
@@ -4,6 +4,7 @@ from enum import Enum
 import pygame
 from pygame_gui import UI_BUTTON_PRESSED, UIManager
 from pygame_gui.elements import UIButton
+from pygame_gui.core import UIElement
 
 from narfecritters.ui.screen import Screen, ScreenManager
 from narfecritters.ui.settings import WINDOW_SIZE
@@ -26,6 +27,7 @@ class BuyScreen(Screen):
         self.screen_manager = screen_manager
         self.world = world
         self.menu_buttons: list[UIButton] = []
+        self.inventory_elements: list[UIElement] = []
         self.init()
 
     def process_event(self, event):
@@ -34,10 +36,11 @@ class BuyScreen(Screen):
                 if event.ui_element.text == MenuOptions.BACK.name:
                     self.screen_manager.pop()
                 if event.ui_element.text == MenuOptions.POTION.name:
-                    if self.world.player.money > COST_POTION:
+                    if self.world.player.money >= COST_POTION:
                         self.world.player.money -= COST_POTION
                         self.world.player.add_item(ItemType.POTION)
                         LOGGER.info("Purchased a potion!")
+                        self.reinit()
 
     def init(self):
         y = WINDOW_SIZE[1] - (len(MenuOptions) + 1) * 32
@@ -47,9 +50,23 @@ class BuyScreen(Screen):
             )
             self.menu_buttons.append(menu_button)
             y += 32
+        self.initialize_inventory_elements()
+
+    def initialize_inventory_elements(self):
+        y = 32
+        for item_type in ItemType:
+            text = f"{item_type.name.capitalize()}: {self.world.player.inventory.get(item_type, 0)}"
+            button = UIButton(
+                relative_rect=pygame.Rect(32, y, WINDOW_SIZE[0] // 3, 32),
+                text=text,
+                manager=self.ui_manager,
+            )
+            self.inventory_elements.append(button)
+            y += 32
 
     def kill(self):
         self.kill_elements(self.menu_buttons)
+        self.kill_elements(self.inventory_elements)
 
     def draw(self, surface: pygame.Surface):
         surface.blit(self.background, (0, 0))
